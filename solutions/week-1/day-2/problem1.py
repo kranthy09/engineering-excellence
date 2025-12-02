4500d599f7ddfff09e4993da85f367de25f7a27e
kranthu <g.kranthi2507@gmail.com>
2025-12-02 19:44:16 +0530

    Week 48 Day 02 progress

---
+++ solutions/week-1/day-2/problem1.py
@@ -0,0 +1,47 @@
+"""
+Subarray Product Less Than K (Medium)
+Given an array of integers nums and an integer k,
+return the number of contiguous subarrays where the
+product of all the elements in the subarray is strictly less than k.
+
+Example 1:
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6]
+Note that [10,5,2] is not included as the product of
+100 is not strictly less than k.
+
+Example 2:
+Input: nums = [1,2,3], k = 0
+Output: 0
+
+Constraints:
+
+1 ≤ nums.length ≤ 3 × 10^4
+1 ≤ nums[i] ≤ 1000
+0 ≤ k ≤ 10^6
+
+"""
+
+
+def solve(nums, k):
+    count = 0
+    n = len(nums)
+    left = 0
+    right = 0
+    prod = 1
+    for right in range(n):
+        prod *= nums[right]
+        while prod >= k:
+            prod //= nums[left]
+            left += 1
+        count += (right-left+1)
+    return count
+
+
+if __name__ == "__main__":
+    nums = [10, 5, 2, 6]
+    k = 100
+    res = solve(nums, k)
+    print(res)
---
+++ solutions/week-1/day-2/problem2.py
@@ -0,0 +1,51 @@
+"""
+ Minimum Size Subarray Sum (Medium)
+Given an array of positive integers nums and a positive integer target,
+ return the minimal length of a subarray whose sum is greater than or
+ equal to target. If there is no such subarray, return 0 instead.
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal
+length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+Constraints:
+
+1 ≤ target ≤ 10^9
+1 ≤ nums.length ≤ 10^5
+1 ≤ nums[i] ≤ 10^4
+
+Follow-up: If you have figured out the O(n) solution,
+try coding another solution of which the time complexity is O(n log n).
+"""
+
+
+def solve(nums, target):
+    n = len(nums)
+    ele_sum = 0
+    left = 0
+    right = 0
+    min_len = float("inf")
+    for right in range(n):
+        ele_sum += nums[right]
+        while ele_sum >= target:
+            min_len = min(min_len, right - left + 1)
+            ele_sum -= nums[left]
+            left += 1
+    return min_len if min_len != float("inf") else 0
+
+
+if __name__ == "__main__":
+    nums = [2, 3, 1, 2, 4, 3]
+    target = 7
+    nums2 = [1, 4, 4]
+    target2 = 4
+    res = solve(nums, target)
+    print(res)
