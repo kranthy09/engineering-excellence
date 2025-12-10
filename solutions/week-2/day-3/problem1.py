c52a0c88638094f9eb874476c52b45012f5b61e4
kranthu <g.kranthi2507@gmail.com>
2025-12-10 18:55:44 +0530

    Week 49 Day 10 progress

--- solutions/week-2/day-3/problem1.py
+++ solutions/week-2/day-3/problem1.py
@@ -44,58 +44,51 @@ s and words[i] consist of lowercase English letters
 All strings in words are the same length
 """
 
+from collections import Counter
+
 
 def solve(s, words):
     """
-    Docstring for solve
-
-    :param s: Description
-    :param words: Description
-
-    TC: O(m*n*word_len)
+    TC: O(m*n)
     SC: O(n)
     """
-    fmap = {}
-    for word in words:
-        if fmap.get(word) is None:
-            fmap[word] = 0
-        fmap[word] += 1
-    lenw = len(words) * len(words[0])
-    lens = len(s)
-    span = len(words[0])
-    s_idx = 0
+    word_len = len(words[0])
+    words_count = len(words)
+    words_freq = Counter(words)
     res = []
-    ans = []
-    while s_idx < lens - lenw + 1:
-        subs = []
-        fmapcp = fmap.copy()
-        for i in range(len(words)):
-            subs.append(s[s_idx+i*span:s_idx+(i+1)*span])
-        for sub in subs:
-            if fmapcp.get(sub) is None:
-                break
+    for offset in range(word_len):
+        left = offset
+        curr_freq = Counter()
+        words_used = 0
+        for right in range(offset, len(s) - word_len + 1, word_len):
+            # construct the word
+            word = s[right: right+word_len]
+            # add word to current window
+            if word in words_freq:  # O(1)
+                curr_freq[word] += 1
+                words_used += 1
+                while curr_freq[word] > words_freq[word]:
+                    left_word = s[left:left+word_len]
+                    curr_freq[left_word] -= 1
+                    words_used -= 1
+                    left += word_len
+                if words_count == words_used:
+                    res.append(left)
+                    # silde window by one word
+                    left_word = s[left:left+word_len]
+                    left += word_len
+                    curr_freq[word] -= 1
+                    words_used -= 1
             else:
-                fmapcp[sub] -= 1
-                if fmapcp[sub] == 0:
-                    del fmapcp[sub]
-        if not fmapcp:
-            ans.append(s_idx)
-            res.append(subs)
-
-        s_idx += 1
-    return ans
+                # restore window
+                curr_freq.clear()
+                words_used = 0
+                left = right + word_len
+        return res
 
 
 if __name__ == "__main__":
     s = "barfoothefoobarman"
     words = ["bar", "foo"]
-    s2 = "wordgoodgoodgoodbestword"
-    words2 = ["word", "good", "best", "word"]
-    s3 = "barfoofoobarthefoobarman"
-    words3 = ["bar", "foo", "the"]
-    res1 = solve(s, words)
-    res2 = solve(s2, words2)
-    res3 = solve(s3, words3)
-    print(res1)
-    print(res2)
-    print(res3)
+    res = solve(s, words)
+    print(res)
