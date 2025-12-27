6362ab6367a05aea98bb42a4776376be15fb59ea
kranthu <g.kranthi2507@gmail.com>
2025-12-28 02:37:09 +0530

    Week 52 Day 28 progress

--- solutions/week-4/day-6/problem3.py
+++ solutions/week-4/day-6/problem3.py
@@ -4,16 +4,21 @@ def count_digits_1_to_n(N):
     n = 1
     i = 1
     res = 0
-    while i < N:
+    while True:
         res += 9*n*i
-        print(res)
         i *= 10
-        if i >= N:
-            print("n, N, i, i//10: ", n, N, i, i//10)
-            res += n*(N-(i//10)+1)
-            return res
         n += 1
+        if i*10 >= N:
+            print("r: ", res)
+            print("s:", n*(N-(i)+1))
+            res += n*(N-(i)+1)
+            return res
+        print(res, i)
+# 0 1 1
+# 9*1*1 10 2
+# 9 + 9*2*10 100 3
+#
 
 
-res = count_digits_1_to_n(125)
+res = count_digits_1_to_n(1121)
 print(res)
