0b382b525095a92451d5ceba34236f3d12c40444
kranthu <g.kranthi2507@gmail.com>
2025-12-30 02:12:22 +0530

    Week 52 Day 30 progress

--- solutions/week-4/day-7/unset_kth_bit.py
+++ solutions/week-4/day-7/unset_kth_bit.py
@@ -7,9 +7,13 @@ output:45
 
 
 def unset_kth_bit(N, k):
+    # unset involves & as it can flip a bit when 1, unchaged when 0.
+    # create mask with 0 at kth bit.
     mask = ~(1 << k)
     return N & mask
 
 
-ans = unset_kth_bit(41, 2)
-print(ans)
+if __name__ == "__main__":
+    N, k = 45, 2
+    ans = unset_kth_bit(N, k)
+    print(ans)  # output: 41
