9f3a4316748d714726c7bad7708749df28276e68
kranthu <g.kranthi2507@gmail.com>
2026-01-15 17:42:44 +0530

    Week 02 Day 15 progress

--- solutions/week-7/day-4/problem1.py
+++ solutions/week-7/day-4/problem1.py
@@ -13,7 +13,7 @@ o/p:
 
 Approaches:
 1. Brute Force: Find all pairs of i, j and return max of j-i
-2. 
+2.
 """
 
 
@@ -36,7 +36,43 @@ class Solution:
         return maxx
 
     def expected_solution(self, *args, **kwargs):
-        pass
+        """
+        maximum j-i can be obtained with samller value on the left
+        greater value on the right. Compute two axuilary arr with
+        min values till i from left, and max value till i from right
+
+        then return first Rmax[0](index) - Lmin[0](index) for Lmin <= Rmin.
+        TC:
+        AS:
+        """
+        arr = args[0]
+        n = len(arr)
+        Lmin = [[0, 0]] * n
+        Rmax = [[0, 0]] * n
+        temp = float("+inf")
+        for i in range(1, n):
+            if temp > arr[i]:
+                Lmin[i][0] = i
+                Lmin[i][1] = arr[i]
+            else:
+                Lmin[i][0] = i
+                Lmin[i][1] = temp
+        temp = float("-inf")
+        for i in range(n, -1, -1):
+            if temp < arr[i]:
+                Rmax[i][0] = i
+                Rmax[i][1] = arr[i]
+            else:
+                Rmax[i][0] = i
+                Rmax[i][1] = temp
+        print(Lmin)
+        print(Rmax)
+        mx = float("-inf")
+        for i in range(n):
+            if Lmin[i] <= Rmax[i]:
+                j_i = Rmax[i][0] - Lmin[i][0]
+                mx = max(mx, j_i)
+        return mx
 
 
 if __name__ == "__main__":
