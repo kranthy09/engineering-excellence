bd1e4eaf16e2ab44e79bf2d1e9aafb327787194e
kranthu <g.kranthi2507@gmail.com>
2026-01-15 21:02:34 +0530

    Week 02 Day 15 progress

--- solutions/week-7/day-4/problem1.py
+++ solutions/week-7/day-4/problem1.py
@@ -12,8 +12,8 @@ i/o:
 o/p:
 
 Approaches:
-1. Brute Force: Find all pairs of i, j and return max of j-i
-2.
+1. Brute Force: Find all pairs of i, j and return max of j-i O(n^2)
+2. Precalculate Lmin + Rmax: O(n)
 """
 
 
@@ -41,49 +41,69 @@ class Solution:
         greater value on the right. Compute two axuilary arr with
         min values till i from left, and max value till i from right
 
-        then return first Rmax[0](index) - Lmin[0](index) for Lmin <= Rmin.
-        TC:
-        AS:
+        in the Left array, starting at 0, count the number of times,
+        it satifies with the condition Lmin[i] <= Rmax[j] such that,
+        the j can stretch up to the condition fails.
+
+        L - minimum till i,
+        R - maximum till n-1 to i
+        A minimum number in L, can form a pair with an element in R,
+        on <= condition, so j is moved where it can go, and can go maximum
+        of its condition to its value j-i.
+        For each iteration, j is increased, till the condition is satisfied,
+        later i is increased and j is continues from the previous index place
+        not from the start, such that L is min of elements which is decreasing
+        so, previous elements in R, will satisfy the condition and now for the
+        current ele from L, will move j forward till the condition is satified.
+        The movement of i, j pointers is crucial as it playes between sorted
+        arrays with a condition to satify.
+        Both pointers are moved forward. Key take way is distance is calculated
+        by moving pointers.
+        TC: O(n)
+        AS: O(n)
         """
         arr = args[0]
         n = len(arr)
-        Lmin = [[0, 0]] * n
-        Rmax = [[0, 0]] * n
+        Lmin = [0] * n
+        Rmax = [0] * n
+
         temp = float("+inf")
-        for i in range(1, n):
-            if temp > arr[i]:
-                Lmin[i][0] = i
-                Lmin[i][1] = arr[i]
-            else:
-                Lmin[i][0] = i
-                Lmin[i][1] = temp
-        temp = float("-inf")
-        for i in range(n, -1, -1):
-            if temp < arr[i]:
-                Rmax[i][0] = i
-                Rmax[i][1] = arr[i]
-            else:
-                Rmax[i][0] = i
-                Rmax[i][1] = temp
-        print(Lmin)
-        print(Rmax)
-        mx = float("-inf")
         for i in range(n):
-            if Lmin[i] <= Rmax[i]:
-                j_i = Rmax[i][0] - Lmin[i][0]
-                mx = max(mx, j_i)
+            if temp > arr[i]:
+                Lmin[i] = arr[i]
+                temp = arr[i]
+            else:
+                Lmin[i] = temp
+        temp = float("-inf")
+        for i in range(n-1, -1, -1):
+            if temp < arr[i]:
+                Rmax[i] = arr[i]
+                temp = arr[i]
+            else:
+                Rmax[i] = temp
+        i = 0
+        j = 0
+        mx = 0
+        while i < n and j < n:
+            if Lmin[i] <= Rmax[j]:
+                mx = max(mx, j-i)
+                j += 1
+            else:
+                i += 1
         return mx
 
 
 if __name__ == "__main__":
     arrs = [
         [34, 8, 10, 3, 2, 80, 30, 33, 1],
+        [2, 3, 2, 5, 23, 54, 12, 7, 8, 1],
+        [9, 4, 8, 16, 19, 2]
     ]
     ans = Solution()
 
     print("******Brute Force******")
     for arr in arrs:
         print(ans.brute_force(arr))
-    print("******Brute Force******")
+    print("******Expected******")
     for arr in arrs:
         print(ans.expected_solution(arr))
