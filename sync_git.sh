86dd43eb9a75803ccf8219b6c43ab5865f7d0316
kranthu <g.kranthi2507@gmail.com>
2026-01-19 20:30:24 +0530

    update

--- sync_git.sh
+++ sync_git.sh
@@ -9,10 +9,8 @@ if [[ -n $(git status -s) ]]; then
     WEEK=$(date +%U)
     DAY=$(date +%d)
     DEFAULT_MSG="Week $WEEK Day $DAY progress"
-
     # Note: Quote "$1" to handle messages with spaces
     MESSAGE=${1:-$DEFAULT_MSG}
-
     git commit -m "$MESSAGE"
     git push
     echo "✓ Changes synced with message: $MESSAGE"
