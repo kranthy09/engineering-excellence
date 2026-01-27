3cb672e913e77c88d3b90f416cf50491e3acc7a4
kranthu <g.kranthi2507@gmail.com>
2026-01-27 18:01:07 +0530

    Week 04 Day 27 progress

---
+++ solutions/week-9/day-1-concept/problem2.py
@@ -0,0 +1,45 @@
+"""
+statement
+
+i/o:
+o/p:
+
+i/o:
+o/p:
+
+Approaches:
+1. Brute Force:
+
+"""
+
+
+class Solution:
+    def brute_force(self, *args, **kwargs):
+        """
+
+        TC:
+        AS:
+        """
+        pass
+
+    def expected_solution(self, *args, **kwargs):
+        """
+
+        TC:
+        AS:
+        """
+        pass
+
+
+if __name__ == "__main__":
+    arrs = [
+        [],
+    ]
+    ans = Solution()
+    print("******Brute Force******")
+    for arr in arrs:
+        print(ans.brute_force(arr))
+    print("******Expected******")
+    for arr in arrs:
+        print(ans.expected_solution(arr))
+
