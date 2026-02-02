5d9f90ca15076a0fb1f3de8e7a795d997cedbd59
kranthu <g.kranthi2507@gmail.com>
2026-02-03 04:08:59 +0530

    Week 05 Day 03 progress

--- solutions/week-10/day-1-concept/ll_palindrome.py
+++ solutions/week-10/day-1-concept/ll_palindrome.py
@@ -1,40 +1,64 @@
 """
-
+Check if linkedlist is a palindrome
 
 i/o: 9 --> 8 --> 7 --> 6 --> 5 --> 3 --> 10 --> None
 o/p: False
 
-i/o: 1 --> 3 --> 6 --> 5 --> 7 --> 9 --> None
-o/p: False
+i/o: 1 --> 3 --> 6 --> 3 --> 6 --> 1 --> None
+o/p: True
 """
 
 
-class Solution:
-    def brute_force(self, *args, **kwargs):
-        """
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def from_list(self, data_list):
+        if not data_list:
+            return None
+
+        # Create the head node
+        self.head = Node(data_list[0])
+        current = self.head
+
+        # Iterate through the rest of the list and link nodes
+        for value in data_list[1:]:
+            current.next = Node(value)
+            current = current.next
+
+        return self.head
+
+    def is_palindrome(self):
+        """
         TC:
         AS:
         """
-        pass
+        state = {"is_pal": True}
+        self.is_palindrome_recr(self.head, state)
+        return state["is_pal"]
 
-    def expected_solution(self, *args, **kwargs):
+    def is_palindrome_recr(self, curr, state):
         """
+        Recursive method to check ll palindrome.
+        """
+        # base condition
+        if curr is None:
+            return self.head
 
-        TC:
-        AS:
-        """
-        pass
+        front = self.is_palindrome_recr(curr.next, state)
+        if curr.data != front.data:
+            state["is_pal"] = False
+        return front.next
 
 
 if __name__ == "__main__":
-    arrs = [
-        [],
-    ]
-    ans = Solution()
-    print("******Brute Force******")
-    for arr in arrs:
-        print(ans.brute_force(arr))
-    print("******Expected******")
-    for arr in arrs:
-        print(ans.expected_solution(arr))
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ll = LinkedList()
+    ll.from_list(arr)
+    print(ll.is_palindrome())
