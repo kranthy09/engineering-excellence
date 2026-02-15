163089934e4a3be68f79f9bd3cbf5bebb83846ae
kranthu <g.kranthi2507@gmail.com>
2026-02-15 16:01:48 +0530

    Week 07 Day 15 progress

---
+++ solutions/recursion_2/unique_subsets.py
@@ -0,0 +1,34 @@
+"""
+Unique subsets
+
+i/o:
+o/p:
+
+i/o:
+o/p:
+
+Approaches:
+1. Brute Force:
+2. Recursion + backtracking
+"""
+
+
+def get_unique_subsets(arr):
+    """
+
+    """
+    curr = []
+    res = []
+    get_unique_subsets_util(0, arr, curr, res)
+    pass
+
+
+def get_unique_subsets_util(i, arr, curr, res):
+    """
+    """
+    pass
+
+
+if __name__ == "__main__":
+    arr = []
+    ans = get_unique_subsets(arr)
