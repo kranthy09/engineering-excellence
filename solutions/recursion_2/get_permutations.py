4b2502771a36c4863d4660614a6130036d97be3c
kranthu <g.kranthi2507@gmail.com>
2026-02-17 23:04:48 +0530

    Get permutations of array of numbers

--- solutions/recursion_2/get_permutations.py
+++ solutions/recursion_2/get_permutations.py
@@ -1,44 +1,58 @@
 """
-statement
+Get Permutations (Easy)
+@topic: recursion_2
+@difficulty: easy
+@tags: recursion, backtracking
 
-i/o:
-o/p:
+Given an array of numbers, generate all
+permutations
 
-i/o:
-o/p:
+i/o: [1, 2, 3]
+o/p:[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
+
 
 Approaches:
-1. Brute Force:
-
+1. Expected: Recursion
 """
 
 
-class Solution:
-    def brute_force(self, *args, **kwargs):
-        """
+def get_permutations_from(arr):
+    """
+    Client for permutaions recursive utility
+    """
+    res = []
+    get_permutations_util(0, arr, res)
 
-        TC:
-        AS:
-        """
-        pass
+    return res
 
-    def expected_solution(self, *args, **kwargs):
-        """
 
-        TC:
-        AS:
-        """
-        pass
+def get_permutations_util(i, arr, res):
+    """
+    at an instant, permutations of i, is
+    swap(i, i) -> keep the element at i, send remaining
+    elements recursively to find out thier permutations
+
+    Do the same step for n numbers in the array as it can swap from i to n
+
+    TC:
+    AS:
+    """
+    # base case
+    if i == len(arr):
+        res.append(arr[:])  # copy of arr
+        return
+
+    for j in range(i, len(arr)):
+        # swap and send remaining numbers to compute permutations.
+        arr[i], arr[j] = arr[j], arr[i]
+        get_permutations_util(i+1, arr, res)
+
+        # backtrack, clean up for next nodes
+        arr[i], arr[j] = arr[j], arr[i]
 
 
 if __name__ == "__main__":
-    arrs = [
-        [],
-    ]
-    ans = Solution()
-    print("******Brute Force******")
-    for arr in arrs:
-        print(ans.brute_force(arr))
-    print("******Expected******")
-    for arr in arrs:
-        print(ans.expected_solution(arr))
+    arr = [1, 2, 3]
+    ans = get_permutations_from(arr)
+    # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
+    print(ans)
