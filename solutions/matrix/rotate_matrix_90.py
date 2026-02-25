ff105b3d49e36c8112084fbc9340f2a85396736b
kranthu <g.kranthi2507@gmail.com>
2026-02-25 17:53:58 +0530

    Rotate matrix 90 clock-wise

--- solutions/matrix/rotate_matrix_90.py
+++ solutions/matrix/rotate_matrix_90.py
@@ -4,12 +4,23 @@ Rotate Matrix 90 (Easy)
 @difficulty: easy
 @tags:
 
-i/o:
-o/p:
+Given a square matrix mat[][] of size n x n.
+The task is to rotate it by 90 degrees in an
+anti-clockwise direction without using any extra space.
+
+
+i/o:   [[0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]]
+
+o/p:   [[2, 5, 8],
+        [1, 4, 7],
+        [0, 3, 6]]
 
 
 Approaches:
-1. Expected: Reverse rows and swap the lower triangle with upper triangle.
+1. Expected_1: Reverse rows and swap the lower triangle with upper triangle.
+2. Expected_2: Actually rotate by swapping the elements.
 """
 
 # TODO: Rotate 90 degrees clock-wise
@@ -17,22 +28,47 @@ Approaches:
 
 class Solution:
 
-    def expected_solution(self, mat):
+    def rotate_matrix_expected_2(self, mat):
         """
-
-        TC:
-        AS:
+        TC: O(n^2)
+        AS: O(1)
         """
         n = len(mat)
-        # step 1 -  reverse the rows
-        # step 2 - swap the lower triangle with upper triangle
-        return n
+        top = 0
+        left = 0
+        right = n - 1
+        bottom = n - 1
+        while top < bottom:
+            # left to right
+            for k in range(left, right):
+                # swap four elements
+                (
+                    mat[top][k],
+                    mat[n - k - 1][left],
+                    mat[bottom][n - k - 1],
+                    mat[k][right],
+                ) = (
+                    mat[k][right],
+                    mat[top][k],
+                    mat[n - k - 1][left],
+                    mat[bottom][n - k - 1],
+                )
+            top += 1
+            bottom -= 1
+            left += 1
+            right -= 1
+        return mat
 
 
 if __name__ == "__main__":
     arrs = [
-        [],
+        [[0, 1, 2],
+         [3, 4, 5],
+         [6, 7, 8]],
     ]
     ans = Solution()
     for arr in arrs:
-        print(ans.expected_solution(arr))
+        print(ans.rotate_matrix_expected_2(arr))
+    # o/p : [[2, 5, 8],
+    #        [1, 4, 7],
+    #        [0, 3, 6]]
