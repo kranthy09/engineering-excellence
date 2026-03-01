ee4fa2753d0c3a1ce351f030558774ca541803fc
kranthu <g.kranthi2507@gmail.com>
2026-03-01 16:16:40 +0530

    Binary Trees Session

---
+++ solutions/BinaryTrees/tree_traversal.py
@@ -0,0 +1,45 @@
+"""
+Tree Traversal (Easy)
+@topic: BinaryTrees
+@difficulty: easy
+@tags: BinaryTree
+
+i/o:
+o/p:
+
+
+Approaches:
+1. Brute Force:
+2. Expected:
+"""
+
+
+class Solution:
+    def brute_force(self):
+        """
+
+        TC:
+        AS:
+        """
+        pass
+
+    def expected_solution(self):
+        """
+
+        TC:
+        AS:
+        """
+        pass
+
+
+if __name__ == "__main__":
+    arrs = [
+        [],
+    ]
+    ans = Solution()
+    print("******Brute Force******")
+    for arr in arrs:
+        print(ans.brute_force(arr))
+    print("******Expected******")
+    for arr in arrs:
+        print(ans.expected_solution(arr))
