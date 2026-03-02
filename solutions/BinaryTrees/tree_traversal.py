83df85bd29a8a9bced1f810557836e0f4985fdd6
kranthu <g.kranthi2507@gmail.com>
2026-03-02 05:31:56 +0530

    binary tree traversals:

--- solutions/BinaryTrees/tree_traversal.py
+++ solutions/BinaryTrees/tree_traversal.py
@@ -38,6 +38,45 @@ def preorder(root):
     preorder(root.right)
 
 
+def postorder(root):
+    """
+    Recursive approach for postorder traversal of
+    binary tree
+
+    order: left, right, self
+
+    TC: O(n)
+    AS: O(H), recursive call stack
+    """
+    # base case
+    if root is None:
+        return
+
+    postorder(root.left)
+    postorder(root.right)
+    print(root.data, end=" ")
+
+
+def inorder(root):
+    """
+    Recursive approach for inorder traversal of
+    binary tree
+
+    order: left, self, right
+
+    TC: O(n)
+    AS: O(H), recursive call stack
+    """
+
+    # base case
+    if root is None:
+        return
+
+    inorder(root.left)
+    print(root.data, end=" ")
+    inorder(root.right)
+
+
 if __name__ == "__main__":
     root = Node(1)
     root.left = Node(20)
@@ -49,3 +88,8 @@ if __name__ == "__main__":
     root.right.right = Node(60)
 
     preorder(root)  # 1 20 40 50 70 80 30 60
+    print()
+    postorder(root)  # 40 70 80 50 20 60 30 1
+    print()
+    inorder(root)
+    print()  # 40 20 70 50 80 1 30 60
